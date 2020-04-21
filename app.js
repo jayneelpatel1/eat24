3f5b913419a3f2b1820cd9a53cd90a0655657a5b
Jayneelkumar Patel <56185597+jayneelpatel1@users.noreply.github.com>
2020-04-21 11:58:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const port = process.env.PORT || 3000
 const express = require('express')
 const app = express()
-const port = 3000
-
 app.get('/', (req, res) => res.send('Hello jayneel  World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
