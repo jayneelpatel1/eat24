e1991d97d360706e15bcf4861ebd5a7b97bbfb06
Jayneelkumar Patel <56185597+jayneelpatel1@users.noreply.github.com>
2020-04-21 11:54:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-const http = require('http');
 const port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-});
+app.get('/', (req, res) => res.send('Hello jayneel  World!'))
 
-server.listen(port, () => {
-    console.log(`Server running at port ` + port);
-});
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
